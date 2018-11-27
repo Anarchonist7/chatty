087fa54b9ff9e667ed70de012c07af792b05abeb
Dawson Ley <anarchonist@protonmail.com>
2018-11-27 21:01:48 +0000

    set initial state and passed props to messagelist

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,35 @@ import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 import NavBar from './Nav.jsx';
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: {name: 'anonymous'},
+      messages: [
+    {
+      id: 1,
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    {
+      id: 2,
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+    }
+  }
+
   render() {
     return (
       <body>
         <NavBar/>
-        <MessageList/>
-        <ChatBar/>
+        <MessageList {...this.state}/>
+        <ChatBar {...this.state}/>
       </body>
     );
   }
 }
+
 export default App;
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
 import Message from './Message.jsx';
+
+
+
 class MessageList extends Component {
+
+
   render () {
+    let some = this.props.messages;
+    let list = some.map((messi) => {
+     return <Message key={messi.id} stuff={messi}/>
+    });
     return (
       <main className="messages">
-        <Message/>
+
+        {list}
         <div className="message system">
           Anonymous1 changed their name to nomnom.
         </div>
