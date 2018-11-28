51f1436436763fba650aba7d50a70b35e5bbe116
Dawson Ley <anarchonist@protonmail.com>
2018-11-28 00:09:16 +0000

    new messages appear in messagelist

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,59 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 import NavBar from './Nav.jsx';
+var inci = 3;
+
 class App extends Component {
   constructor(props) {
     super(props);
 
+    this.handler = this.handler.bind(this);
+
     this.state = {
       currentUser: {name: 'anonymous'},
       messages: [
-    {
-      id: 1,
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-    },
-    {
-      id: 2,
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-    }
-  ]
+        {
+          id: 1,
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          id: 2,
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
     }
   }
+
+  handler(newMsg) {
+    var idee = inci++;
+    const newMessage = {id: idee, username: 'Bob', content: newMsg};
+    const messages = this.state.messages.concat(newMessage);
+    this.setState({
+      messages: messages
+    });
+  }
+
+  // componentDidMount() {
+  //   console.log("componentDidMount <App />");
+  //   setTimeout(() => {
+  //     console.log("Simulating incoming message");
+  //     // Add a new message to the list of messages in the data store
+  //     const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+  //     const messages = this.state.messages.concat(newMessage)
+  //     // Update the state of the app component.
+  //     // Calling setState will trigger a call to render() in App and all child components.
+  //     this.setState({messages: messages})
+  //   }, 3000);
+  // }
 
   render() {
     return (
       <body>
         <NavBar/>
         <MessageList {...this.state}/>
-        <ChatBar {...this.state}/>
+        <ChatBar handler={this.handler} {...this.state}/>
       </body>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+
   render () {
+    const onKeyPress = evt => {
+
+      if (evt.key === 'Enter') {
+        evt.preventDefault();
+        const chatMessageInput = evt.target;
+
+      // Here, we call the function we were sent
+        this.props.handler(chatMessageInput.value);
+
+        chatMessageInput.value = '';
+      }
+
+//makes another function to change chatname
+    };
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder={this.props.currentUser.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input name="chatName" className="chatbar-username" placeholder={this.props.currentUser.name} />
+        <input onKeyPress={onKeyPress} className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     )
   }
